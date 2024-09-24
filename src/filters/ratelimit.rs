8dd60d1f27b7e59a5dc6894f08ddba83c2cf3050
Daniel Saxton <dsaxton@pm.me>
2024-09-23 21:23:51 -0500

    Make rate limiting message configurable

--- src/filters/ratelimit.rs
+++ src/filters/ratelimit.rs
@@ -12,6 +12,7 @@ pub struct Tokens {
 pub struct RateLimit {
     pub posts_per_minute: i32,
     pub whitelist: Option<Vec<String>>,
+    pub message: String,
 
     #[serde(skip)]
     pub sources: HashMap<String, Tokens>,
@@ -65,7 +66,7 @@ impl NoteFilter for RateLimit {
             return OutputMessage::new(
                 msg.event.id.clone(),
                 Action::Reject,
-                Some("rate-limited: you are noting too much".to_string()),
+                Some(self.message.clone()),
             );
         }
 
